d00465a9bf125f631abedc1cc036c0ab69c0e48e
derekangziying <derekangziying@gmail.com>
2020-09-25 13:20:42 +0800

    Make raw login page

--- src/components/layout/HeaderOptions.js
+++ src/components/layout/HeaderOptions.js
@@ -3,14 +3,28 @@ import { useHistory } from "react-router-dom";        //react hook.
 import UserContext from "../../context/UserContext";
 
 export default function HeaderOptions() {
+  const { userData, setUserData } = useContext(UserContext); 
+
+  const history = useHistory();                      
+
+  const register = () => history.push("/register");  
+  const login = () => history.push("/login");         
+  const logout = () => {                             
+    setUserData({                                    
+      token: undefined,
+      user: undefined,
+    });
+    localStorage.setItem("auth-token", ""); 
+  };
+
   return (
     <nav className="header-options">
       {false ? (
-        <button onClick={null}>Log out</button>
+        <button onClick={logout}>Log out</button>
       ) : (                                              
         <>
-          <button onClick={null}>Register</button>
-          <button onClick={null}>Log in</button>
+          <button onClick={register}>Register</button>
+          <button onClick={login}>Log in</button>
         </>
       )}
     </nav>
